1368174151ee8505ba65a6bff8305ca568327129
Spoon <coatesjak@gmail.com>
2023-10-24 04:27:04 -0400

    Fixes fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,10 @@ game 'gta5'
 author 'Spoontipu'
 description 'Criminal art heist'
 
-client_script 'client/client.lua'
+client_script {
+    'client/client.lua',
+    'client/events.lua',
+    'client/functions.lua',
+}
 server_script 'server/server.lua'
 shared_script 'config.lua'
